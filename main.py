a93a3a9e5447399092043906379c44c13cc9c6a8
Tanyu Brandon <82066029+tanyubrandon@users.noreply.github.com>
2022-10-28 23:42:27 +0000

    Formatted printing added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print(""" 
 Tanyu
- October 22th 
-
+October 22th 
+I am Signing up for Replit's 100days of python challenge!
+I will make sure to spend some time every day coding along, for a minimun of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone where ever i happen to be. No excuses for not coding from the moddle of the field.
+I am feeling 😇 
+ 
 """)
