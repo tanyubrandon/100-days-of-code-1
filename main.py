4b39078f0aa722261033d5d7877a83a5969bb2bd
Tanyu Brandon <82066029+tanyubrandon@users.noreply.github.com>
2022-10-28 23:45:18 +0000

    replit  username added to the print statement

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ I am Signing up for Replit's 100days of python challenge!
 I will make sure to spend some time every day coding along, for a minimun of 10 minutes a day.
 I'll be using Replit, an amazing online IDE so I can do this from my phone where ever i happen to be. No excuses for not coding from the moddle of the field.
 I am feeling 😇 
+My replit username is @tanyubrandon
  
 """)
